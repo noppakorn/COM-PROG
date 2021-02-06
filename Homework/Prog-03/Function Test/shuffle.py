a84a6232669556c1e9943a6dfd3a0ff691a4b346
Noppakorn Jiravaranun <meen2545@gmail.com>
2021-02-07 01:08:06 +0700

    Add shuffle test cases

--- Homework/Prog-03/Function Test/shuffle.py
+++ Homework/Prog-03/Function Test/shuffle.py
@@ -15,8 +15,12 @@ assert shuffle(odeck) == "|2H||9H||3H||TH||4H||JH||5H||QH||6H||KH||7H||AH||8H|"
 assert shuffle(edeck) == "|2H||8H||3H||9H||4H||TH||5H||JH||6H||QH||7H||KH|"
 
 #special test
+odeck1 = "|2H|"
+odeck2 = "|2H||3H|"
 odeck3 = "|2H||3H||4H|"
 odeck5 = "|2H||3H||4H||5H||6H|"
 print('-------------')
+print(shuffle(odeck1))
+print(shuffle(odeck2))
 print(shuffle(odeck3))
 print(shuffle(odeck5))

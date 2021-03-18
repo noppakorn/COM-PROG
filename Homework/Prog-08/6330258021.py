e49faf6c7c4042a4441c0c5af351d3badaee291c
Noppakorn <meen2545@gmail.com>
2021-03-18 14:51:02 +0700

    Prog-08: Refactor

--- Homework/Prog-08/6330258021.py
+++ Homework/Prog-08/6330258021.py
@@ -4,9 +4,9 @@
 def readlines(fn) :
     fin = open(fn,'r')
     lines,line = [],fin.readline().strip()
-    while line != '' :
-        lines.append(line)
-        line = fin.readline().strip()        
+    while len(line) != 0 :
+        lines.append(line.strip())
+        line = fin.readline()        
     fin.close()
     return lines
 
@@ -15,7 +15,13 @@ def getstopl(fn) :
     for i in readlines(fn) : l += i.split()
     return l
 
-def countandwords(lines) :
+def fhash(w,m) :
+    h = 0
+    for i in range(len(w)) : h += ord(w[i])*(37**i)
+    return h % m
+
+def getinfo(fn) :
+    lines = readlines(fn)
     cc,ca,s = 0,0,''
     for line in lines : 
         cc += len(line)
@@ -24,21 +30,17 @@ def countandwords(lines) :
                 s += c
                 ca += 1
             else : s += ' '
-    return cc,ca,s.split()
-
-def fhash(w,m) :
-    h = 0
-    for i in range(len(w)) : h += ord(w[i])*(37**i)
-    return h % m
-
-def getinfo(fn) :
-    lines = readlines(fn)
-    cc,ca,s = countandwords(lines)
+    s = s.split()
     print('char count = %d' % cc) 
     print('alphanumeric count = %d' % ca)
     print('line count = %d' % len(lines))
     print('word count = %d' % len(s))
     return s
+
+def print_bow(uword,cword) :
+    bow = []
+    for i in range(len(uwords)) : bow.append([uwords[i],cwords[i]])
+    print('BoW = %s' % sorted(bow))
 
 file_name = input('File name = ').strip()
 mode = input('Use feature hashing ? (y,Y,n,N) ').strip().lower()
@@ -57,10 +59,7 @@ if mode == 'n' :
             uwords.append(word)
             cwords.append(1)
         else : cwords[uwords.index(word)] += 1
-    bow = []
-    for i in range(len(uwords)) :
-        bow.append([uwords[i],cwords[i]])
-    print('BoW = %s' % sorted(bow))
+    print_bow(uwords,cwords)
 
 elif mode == 'y' :
     m = int(input('M = ').strip())
@@ -74,7 +73,4 @@ elif mode == 'y' :
             uwords.append(hword)
             cwords.append(1)
         else : cwords[uwords.index(hword)] += 1
-    bow = []
-    for i in range(len(uwords)) :
-        bow.append([uwords[i],cwords[i]])
-    print('BoW = %s' % sorted(bow))
+    print_bow(uwords,cwords)

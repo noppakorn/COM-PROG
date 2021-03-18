051a939b36a84efc91733fab89d0cd7a1da0aa41
Noppakorn <meen2545@gmail.com>
2021-03-18 16:59:22 +0700

    Prog-08: Refactor

--- Homework/Prog-08/6330258021.py
+++ Homework/Prog-08/6330258021.py
@@ -10,11 +10,6 @@ def readlines(fn) :
     fin.close()
     return lines
 
-def getstopl(fn) :
-    l = []
-    for i in readlines(fn) : l += i.split()
-    return l
-
 def fhash(w,m) :
     h = 0
     for i in range(len(w)) : h += ord(w[i])*(37**i)
@@ -37,40 +32,23 @@ def getinfo(fn) :
     print('word count = %d' % len(s))
     return s
 
-def print_bow(uword,cword) :
-    bow = []
-    for i in range(len(uwords)) : bow.append([uwords[i],cwords[i]])
-    print('BoW = %s' % sorted(bow))
-
 file_name = input('File name = ').strip()
 mode = input('Use feature hashing ? (y,Y,n,N) ').strip().lower()
 while mode != 'n' and mode != 'y' :
     print('Try again.')
     mode = input('Use feature hashing ? (y,Y,n,N) ').strip().lower()
 print('-------------------')
-
-if mode == 'n' :
-    uwords,cwords = [],[]
-    stopwords = getstopl('stopwords.txt')
-    for word in getinfo(file_name) :
-        word = word.lower()
-        if word in stopwords  : continue
-        elif word not in uwords : 
-            uwords.append(word)
-            cwords.append(1)
-        else : cwords[uwords.index(word)] += 1
-    print_bow(uwords,cwords)
-
-elif mode == 'y' :
-    m = int(input('M = ').strip())
-    uwords,cwords = [],[]
-    stopwords = getstopl('stopwords.txt')
-    for word in getinfo(file_name) :
-        word = word.lower()
-        if word in stopwords  : continue
-        hword = fhash(word,m)
-        if hword not in uwords : 
-            uwords.append(hword)
-            cwords.append(1)
-        else : cwords[uwords.index(hword)] += 1
-    print_bow(uwords,cwords)
+if mode == 'y' : m = int(input('M = ').strip())
+uwords,cwords,stopwords = [],[],[]
+for i in readlines('stopwords.txt') : stopwords += i.split()
+for word in getinfo(file_name) :
+    word = word.lower()
+    if word in stopwords  : continue
+    if mode == 'y' : word = fhash(word,m)
+    if word not in uwords : 
+        uwords.append(word)
+        cwords.append(1)
+    else : cwords[uwords.index(word)] += 1
+bow = []
+for i in range(len(uwords)) : bow.append([uwords[i],cwords[i]])
+print('BoW = %s' % sorted(bow))

640d2762a8bed0612b6140bc723f5bf1a3b57379
Noppakorn <meen2545@gmail.com>
2021-03-18 14:50:46 +0700

    08_Dict : Added 12

---
+++ Grader_Exercise/08_Dict/08_Dict_12.py
@@ -0,0 +1,10 @@
+nani,nina = {},{}
+for i in range(int(input())) :
+    x = input().split()
+    nani[x[0]] = x[1]
+    nina[x[1]] = x[0]
+for i in range(int(input())) :
+    y = input()
+    if y in nani.keys() : print(nani[y])
+    elif y in nina.keys() : print(nina[y])
+    else : print('Not found')

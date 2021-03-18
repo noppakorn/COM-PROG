83c420c2d39d06d753593ff5ee45273510a524b1
Noppakorn <meen2545@gmail.com>
2021-03-18 17:14:03 +0700

    Prog-08: Fix output formatting

--- Homework/Prog-08/6330258021.py
+++ Homework/Prog-08/6330258021.py
@@ -15,33 +15,25 @@ def fhash(w,m) :
     for i in range(len(w)) : h += ord(w[i])*(37**i)
     return h % m
 
-def getinfo(fn) :
-    lines = readlines(fn)
-    cc,ca,s = 0,0,''
-    for line in lines : 
-        cc += len(line)
-        for c in line :
-            if c.isalnum() : 
-                s += c
-                ca += 1
-            else : s += ' '
-    s = s.split()
-    print('char count = %d' % cc) 
-    print('alphanumeric count = %d' % ca)
-    print('line count = %d' % len(lines))
-    print('word count = %d' % len(s))
-    return s
-
 file_name = input('File name = ').strip()
 mode = input('Use feature hashing ? (y,Y,n,N) ').strip().lower()
 while mode != 'n' and mode != 'y' :
     print('Try again.')
     mode = input('Use feature hashing ? (y,Y,n,N) ').strip().lower()
-print('-------------------')
 if mode == 'y' : m = int(input('M = ').strip())
-uwords,cwords,stopwords = [],[],[]
+print('-------------------')
+lines = readlines(file_name)
+cc,ca,words = 0,0,''
+for line in lines : 
+    cc += len(line)
+    for c in line :
+        if c.isalnum() : 
+            words += c
+            ca += 1
+        else : words += ' '
+words,uwords,cwords,stopwords = words.split(),[],[],[]
 for i in readlines('stopwords.txt') : stopwords += i.split()
-for word in getinfo(file_name) :
+for word in words :
     word = word.lower()
     if word in stopwords  : continue
     if mode == 'y' : word = fhash(word,m)
@@ -51,4 +43,8 @@ for word in getinfo(file_name) :
     else : cwords[uwords.index(word)] += 1
 bow = []
 for i in range(len(uwords)) : bow.append([uwords[i],cwords[i]])
+print('char count = %d' % cc) 
+print('alphanumeric count = %d' % ca)
+print('line count = %d' % len(lines))
+print('word count = %d' % len(words))
 print('BoW = %s' % sorted(bow))

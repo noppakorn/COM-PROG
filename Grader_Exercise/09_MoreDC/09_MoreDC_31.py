5ea1c0432ba0c40fcf20d340e85b2fda946fcf67
Noppakorn Jiravaranun <meen2545@gmail.com>
2021-04-03 15:42:57 +0700

    09_MoreDC: Added non-working 31

--- Grader_Exercise/09_MoreDC/09_MoreDC_31.py
+++ Grader_Exercise/09_MoreDC/09_MoreDC_31.py
@@ -3,9 +3,10 @@ def gcd(a,b):
         a,b = b, a%b
     return a
 def is_coprime(a, b, c):
- # คืนผลการทดสอบว่า a, b และ c เป็น coprime หรือไม่
- # อ่านนิยาม coprime ที่ https://en.wikipedia.org/wiki/Coprime_integers
-    pass
+    if gcd(a,b) == 1: return True
+    if gcd(a,c) == 1: return True
+    if gcd(c,b) == 1: return True
+    return False
 def primitive_Pythagorean_triples(max_len):
  # คืนลิสต์ ทภี่ ายในเกบ็ ลสิตย์ อ่ ยทมี่ สี มาชกิสามคา่ ของ a, b และ c
  # โดยที่ a  b  c  max_len
@@ -15,7 +16,15 @@ def primitive_Pythagorean_triples(max_len):
  # [20, 21, 29], [12, 35, 37], [9, 40, 41], [28, 45, 53],
  # [11, 60, 61], [16, 63, 65], [33, 56, 65]]
     triple = []
-    pass
-    return triple
-
-exec(input().strip())
+    i = 0
+    while True:
+        m,n = 2*i+3,2*i+1
+        if is_coprime(m,n):
+            a = m*n
+            b = (m**2-n**2)//2
+            c = (m**2+n**2)//2
+            if a**2 + b**2 == c**2: triple.append([a,b,c])
+            if c>max_len: break
+        i += 1
+print(primitive_Pythagorean_triples(65))
+#exec(input().strip())

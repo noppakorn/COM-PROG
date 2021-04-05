d8a1f5ae998a9ce815f974ba9e5c62766ea19360
Noppakorn <meen2545@gmail.com>
2021-04-05 17:14:22 +0700

    Prog-10: Added two test to testcases

---
+++ Homework/Prog-10/testcases/Prog-10-test.py
@@ -0,0 +1,30 @@
+import glob
+errors = []
+file_in = 'stressgene.png'
+print('----------Case 1----------')
+print('Testing embeding text file and reading it back')
+for txt in glob.glob('*.txt'):
+    print('********',txt)
+    rtxt = open(txt).read()
+    try :
+        assert embed_text_to_image(rtxt,file_in,'%s_%s.png' % (file_in[:-4],txt[:-4])) == True
+        get = get_embedded_text_from_image('%s_%s.png' % (file_in[:-4],txt[:-4]))
+        if txt == 'disclaimer.txt' :print(get)
+        assert get == rtxt
+    except: 
+        errors.append('Case 1 : ' + txt)
+print('----------Case 2----------')
+print('Testing not large enough images')
+file_in = '5x5_none0.png'
+for txt in glob.glob('*.txt'):
+    print('********',txt)
+    rtxt = open(txt).read()
+    try :
+        assert embed_text_to_image(rtxt,file_in,'%s_%s.png' % (file_in[:-4],txt[:-4])) != True 
+    except: 
+        errors.append('Case 2 : ' + txt)
+print('---------------------------------------------------------')
+if len(errors) > 0 :
+    print('Errors : ') 
+    for i in errors : print(i)
+else : print('All tests passes! Say Thanks!')
